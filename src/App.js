be69eefaea46dff4f35a0e6309a7f0b81c94e57d
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-10-03 13:22:32 +0100

    created component 'Paragraph'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header";
+import Header from './components/Header';
+import Paragraph from './components/Paragraph';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Paragraph />
     </>
   );
 }
