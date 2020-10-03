f3094b78700ab77d76de410eba6021afcddab420
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-10-03 13:26:44 +0100

    created component 'Square'

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Paragraph from './components/Paragraph';
+import Square from './components/Square';
 
 const App = () => {
   return (
     <>
       <Header />
       <Paragraph />
+      <Square />
     </>
   );
 }
---
+++ src/components/Square/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Square = () => {
+    return (
+        <div
+            style={{ 
+                height: "200px",
+                width: "200px",
+                backgroundColor: "red"
+             }}>
+        </div>
+    )
+}
+
+export default Square;
