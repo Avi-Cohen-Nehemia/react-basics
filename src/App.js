4a2a6ef54a3ab322f026dfe9cdeeecc603d682db
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-10-03 13:38:42 +0100

    moved all the components into a component called 'Stuff' and imported it to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Paragraph from './components/Paragraph';
-import People from './components/People';
-import Square from './components/Square';
+import Stuff from './components/Stuff';
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Paragraph />
-      <Square />
-      <People />
+      <Stuff />
     </>
   );
 }
---
+++ src/components/Stuff/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from './../Header'
+import Square from './../Square'
+import Paragraph from './../Paragraph'
+import People from './../People'
+
+const Stuff = () => {
+    return (
+        <>
+            <Header />
+            <Paragraph />
+            <Square />
+            <People />
+        </>
+    )
+}
+
+export default Stuff;
