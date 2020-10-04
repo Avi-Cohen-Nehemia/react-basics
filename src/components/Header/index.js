6e83eadc8cc34705fe808984cba920f2ec4f4fd0
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-10-04 13:13:26 +0100

    introduced propTypes to 'Header' component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import propTypes from 'prop-types';
 
 const Header = ({ text }) => {
     return (
         <h1 className="alert alert-primary">{ text }</h1>
     )
+}
+
+Header.propTypes = {
+    text: propTypes.string,
 }
 
 export default Header;
