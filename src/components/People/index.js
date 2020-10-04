366f26bcae7b322775b9d5a4bdefae8fd2abb504
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-10-04 13:56:14 +0100

    People has explicit return statement

--- src/components/People/index.js
+++ src/components/People/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import propTypes from 'prop-types';
 
-const People = ({ names }) => (
-    names ? (
-    <ul className="list-group">
-        { names.map((name, index) => (
-            <li className="list-group-item" key={ index }>{ name }</li>
-        ))}
-    </ul>
-    ) : <p>Nothing to see here</p>
-)
+const People = ({ names }) => {
+    return (
+        names ? (
+        <ul className="list-group">
+            { names.map((name, index) => (
+                <li className="list-group-item" key={ index }>{ name }</li>
+            ))}
+        </ul>
+        ) : <p>Nothing to see here</p>
+    )
+}
 
 People.propTypes = {
     names: propTypes.array,
