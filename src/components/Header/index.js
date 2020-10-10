b497c587749b6452ed67c5a3a390a5443089e959
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-10-10 14:57:58 +0100

    added comments

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,10 @@ class Header extends Component {
     constructor(props) {
         // super props is a way for us to have access to props within the constructor method
         super(props);
+
         // state is a way for us to keep track of the changes happening to our component
         this.state = { counter: 0 }
+
         // all the methods we create have to bind in our constructor in order to work
         this.handleClick = this.handleClick.bind(this);
     }
@@ -14,19 +16,24 @@ class Header extends Component {
     handleClick() {
         // we don't have to but it is good practice to store our state in variables before using them in setState
         let currentCounter = this.state.counter
+
+        // we have to use the setState method to make react rerender our component and show us the change of our state
+        // setState will always be the last thing to run in our method
         this.setState({counter: currentCounter + 1});
     }
 
     render() {
         // we can destructur props inside the render method when using class based components
         const { children } = this.props;
+        
         // we can destructur state inside the render method when using class based components
         const { counter } = this.state;
 
         return (
+            // pass methods into events
             <header onClick={this.handleClick}>
                 <h1 className="alert alert-primary">{children}</h1>
-                <p>{counter}</p>
+                <p>Counter: {counter}</p>
             </header>
         );
     }
