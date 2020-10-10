609445069bf56ac968cb2d0f7d554ae9dccf68c3
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-10-10 14:48:23 +0100

    make header keep track of the amount of times it is clicked

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,18 +7,25 @@ class Header extends Component {
         super(props);
         // state is a way for us to keep track of the changes happening to our component
         this.state = { counter: 0 }
+        // all the methods we create have to bind in our constructor in order to work
+        this.handleClick = this.handleClick.bind(this);
     }
     // a method we can pass to our event handlers in the render method
     handleClick() {
-        console.log("hi");
+        let currentCounter = this.state.counter
+        this.setState({counter: currentCounter + 1});
     }
 
     render() {
         // we can destructur props inside the render method when using class based components
         const { children } = this.props;
+        // we can destructur state inside the render method when using class based components
+        const { counter } = this.state;
+
         return (
             <header onClick={this.handleClick}>
                 <h1 className="alert alert-primary">{children}</h1>
+                <p>{counter}</p>
             </header>
         );
     }
