00bcbc834c46a4be8e229db36ba1404801086e35
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-10-10 14:22:22 +0100

    convert onClick to use handle method

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import PropTypes from 'prop-types';
 
 class Header extends Component {
+    handleClick() {
+        console.log("hi")
+    }
+
     render() {
         // we can destructur props inside the render method when using class based components
         const { children } = this.props;
         return (
-            <header onClick={() => console.log("hi")}>
+            <header onClick={this.handleClick}>
                 <h1 className="alert alert-primary">{children}</h1>
             </header>
         )
