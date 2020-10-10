249052c0981777d577f22ca74c7f53b7e49ceae6
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-10-10 14:38:51 +0100

    add state to Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,15 @@ import React, { Component } from "react";
 import PropTypes from 'prop-types';
 
 class Header extends Component {
+    constructor(props) {
+        // super props is a way for us to have access to props within the constructor method
+        super(props);
+        // state is a way for us to keep track of the changes happening to our component
+        this.state = { counter: 0 }
+    }
+    // a method we can pass to our event handlers in the render method
     handleClick() {
-        console.log("hi")
+        console.log("hi");
     }
 
     render() {
@@ -13,7 +20,7 @@ class Header extends Component {
             <header onClick={this.handleClick}>
                 <h1 className="alert alert-primary">{children}</h1>
             </header>
-        )
+        );
     }
 }
 
