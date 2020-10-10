c117fb5f8f50a7c41f8ac4c97f435a9683fadb35
Avi Cohen Nehemia <avrahamhcn@gmail.com>
2020-10-10 15:09:15 +0100

    convert Paragraph component into a class based component

--- src/components/Paragraph/index.js
+++ src/components/Paragraph/index.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from 'prop-types';
 
-const Paragraph = ({ children }) => {
-    return (
-        <p>{ children }</p>
-    )
+class Paragraph extends Component {
+    render() {
+        const { children } = this.props;
+        return (
+            <p>{children}</p>
+        );
+    }
 }
 
 Paragraph.defaultProps = {
